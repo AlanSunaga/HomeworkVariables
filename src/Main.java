731448dcdd10f77031644753ab4022ff2ed1928c
AlanSunaga <Shumvavi@mail.ru>
2025-03-27 14:18:44 +0300

    Сделал

--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,140 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+    //Задача 1
+        //Необходимо присвоить переменным разные значения. Для решения этой задачи используйте тип переменной var.
+        //
+        //В программе IDEA в новом созданном вами проекте присвойте:
+        //
+        //переменной dog значение 8.0;
+        //переменной cat значение 3.6;
+        //переменной paper значение 763789.
+        //Выведите значения каждой переменной в консоль.
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        System.out.println("Задача 1");
+        double dog = 8;
+        var cat = 3.6;
+        var paper = 763789;
+        System.out.println(dog);
+        System.out.println(cat);
+        System.out.println(paper);
+        System.out.println();
+
+    //Задача 2
+        //Увеличьте значение каждой перечисленной в прошлой задаче переменной на 4. Для решения этой задачи используйте тип переменной var.
+        //
+        //Выведите в консоль новые значения переменных.
+
+        System.out.println("Задача 2");
+        dog = 8 + 4;
+        cat = 3.6 + 4;
+        paper = 763789 + 4;
+        System.out.println(dog);
+        System.out.println(cat);
+        System.out.println(paper);
+        System.out.println();
+
+    //Теперь нужно уменьшить значение каждой переменной:
+        //
+        // dog уменьшить на 3.5;
+        // cat уменьшить на 1.6;
+        // paper уменьшить на 7639.
+        //Выведите в консоль новые значения переменных. Для решения этой задачи используйте тип переменной var.
+
+        System.out.println("Задача 3");
+        dog = dog - 3.5;
+        cat = 3.6 - 1.6;
+        paper = 763789 - 7639;
+        System.out.println(dog);
+        System.out.println(cat);
+        System.out.println(paper);
+        System.out.println();
+
+    //Задача 4
+        //Инициализируйте (присвойте значение) переменную friend значением 19.
+        //
+        //Увеличьте значение переменной на 2, после чего поделите значение на 7.
+        //
+        //После каждой операции выводите значение переменной в консоль. В итоге у вас должно быть выведено 3 значения одной переменной.
+        //
+        //Для решения этой задачи используйте тип переменной var.
+
+        System.out.println("Задача 4");
+        var friend = 19;
+        System.out.println(friend);
+        friend = friend + 2;
+        System.out.println(friend);
+        friend = friend / 7;
+        System.out.println(friend);
+        System.out.println();
+
+    //Задача 5
+        //Инициализируйте переменную frog значением 3.5.
+        //
+        //Увеличьте переменную в 10 раз и поделите на 3.5. Добавьте к последнему значению переменной 4.
+        //
+        //После каждой операции выводите значение переменной в консоль. В итоге у вас должно быть выведено 4 значения одной переменной.
+        //
+        //Для решения этой задачи используйте тип переменной var.
+
+        System.out.println("Задача 5");
+        var frog = 3.5;
+        System.out.println(frog);
+        frog = frog * 10;
+        System.out.println(frog);
+        frog = frog / 3.5;
+        System.out.println(frog);
+        frog = frog + 4;
+        System.out.println(frog);
+        System.out.println();
+
+    //Задача 6
+        //В боксе перед каждым боем спортсменов взвешивают. Это делают для того, чтобы убедиться, что боксеры соответствуют своей весовой категории и бой будет честным.
+        //
+        //Масса одного боксера — 78.2 кг.
+        //
+        //Масса второго боксера — 82.7 кг.
+        //
+        //Подсчитайте и выведите в консоль общую массу двух бойцов.
+        //
+        //Подсчитайте и выведите в консоль разницу между массами бойцов. Для решения этой задачи используйте тип переменной var.
+
+        System.out.println("Задача 6");
+        var menOne = 78.2;
+        var menTwo = 82.7;
+        var result = menOne + menTwo;
+        System.out.println("Общая масса двух бойцов - " + result + "кг");
+        var difference = menTwo - menOne;
+        System.out.println("Разница в весе между бойцами - " + difference + "кг");
+        System.out.println();
+
+    //Задача 7
+        //Найдите остаток от деления между двумя весами. Для решения этой задачи используйте тип переменной var.
+
+        System.out.println("Задача 7");
+        difference = menTwo % menOne;
+        System.out.println("Разница в весе между бойцами - " + difference + "кг");
+        System.out.println();
+
+    //Задача 8
+        //Решите задачу с помощью арифметических функций.
+        //
+        // 640 часов работы поделено между сотрудниками. Если каждый сотрудник посвящает работе 8 часов,
+        // то сколько всего работников в компании? Выведите результат задачи в консоль в формате: «Всего работников в компании — … человек».
+
+        // Посчитайте, сколько часов работы должно быть поделено между сотрудниками, если в компании работает на 94 человека больше.
+        // Выведите результат задачи в консоль в формате: «Если в компании работает … человек, то всего … часов работы может быть поделено между сотрудниками».
+        //Для решения этой задачи используйте тип переменной var.
+
+        System.out.println("Задача 8");
+        var allHours = 640;
+        var personHours = 8;
+        var allPerson = allHours / personHours;
+        System.out.println("Всего работников в компании — " + allPerson + " человек");
+
+        var newAllperson = allPerson + 94;
+        var newAllhours = newAllperson * 8;
+        System.out.println("Если в компании работает " + newAllperson + " человек, то всего " + newAllhours + " часов работы может быть поделено между сотрудниками.");
+
     }
 }
